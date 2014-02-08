d3bc878b73f526e9d3baef5c9bb315ea1721e97f
蒼時弦也 <elct9620@frost.tw>
2014-02-08 16:48:24 +0800

    Update# add speaker and session data

--- view_helpers.rb
+++ view_helpers.rb
@@ -1,6 +1,19 @@
 module ViewHelpers
+  def load_yaml(file)
+    YAML.load_file(File.join(File.dirname(__FILE__), file))
+  end
   def config
-    data ||= YAML.load_file(File.join(File.dirname(__FILE__), "config.yml"))
+    data ||= load_yaml("config.yml")
+    data
+  end
+
+  def sessions
+    data ||= load_yaml("sessions.yml")
+    data
+  end
+
+  def speakers
+    data ||= load_yaml("speakers.yml")
     data
   end
 
@@ -14,5 +27,17 @@ module ViewHelpers
 
   def bower_tag(path)
     content_tag :script, "", :src => "bower_components/#{path}.js"
+  end
+
+  # Relative version image tag
+  def image_tag(src, html_options = {})
+    src = "#{Compass.configuration.images_dir}/#{src}" unless src =~ /^(https?:|\/)/
+    tag(:img, html_options.merge(:src=>src))
+  end
+
+  def avatar_image(avatar_url, size = 150)
+    avatar_url = "http://www.gravatar.com/avatar/?d=mm&s=#{size}" if avatar_url.nil?
+    avatar_url = "http://www.gravatar.com/avatar/#{avatar_url[2..-1]}?d=mm&s=#{size}" if avatar_url =~ /^(g:)/
+    image_tag(avatar_url)
   end
 end
